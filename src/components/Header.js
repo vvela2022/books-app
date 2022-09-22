91ed8773381073f3d3db04a3a2e68bd21507db24
“vvela2022” <victoriavela01@gmail.com>
2022-09-22 11:41:33 -0500

    colors and font complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ const Header = () => {
     return(
     <div className='Header'>
         <div className='header-main'>
-            <h1>Books2Read</h1>
-            <p>A place to browse the NY times best-seller lists and decide what to read next. </p>
+            <div className= 'content-main'>
+                <h1>Books2Read</h1>
+                <p>A place to browse the NY times best-seller lists and decide what to read next. </p>
+            </div>
         </div>
         <Nav />
     </div>
